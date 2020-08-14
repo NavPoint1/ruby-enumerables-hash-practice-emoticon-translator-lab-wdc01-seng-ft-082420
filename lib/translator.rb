460814e9f142bcb7887b7d50fab20f0774c0183e
outstanding-publisher-2341 <zaxmaf@gmail.com>
2020-08-14 19:47:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,9 +14,9 @@ end
 
 def get_japanese_emoticon(path, eng_emoticon)
   # code goes here
-  library = load_library(path)
+  #library = load_library(path)
   
-  jpn_emoticon = 
+  #jpn_emoticon = 
 end
 
 def get_english_meaning(path, jpn_emoticon)
