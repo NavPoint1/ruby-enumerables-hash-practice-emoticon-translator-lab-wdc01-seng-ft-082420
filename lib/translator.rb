23cc48029c7b3a3cc2bae220551fbff11689e78f
outstanding-publisher-2341 <zaxmaf@gmail.com>
2020-08-14 19:35:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,8 +4,12 @@ def load_library(path)
   # code goes here
   library = YAML.load_file(path)
   library.map { |key, value|
-    library[key] = 
+    library[key] = {
+      english: library[key][0],
+      japanese: library[key][1]
+    }
   }
+  p library
 end
 
 def get_japanese_emoticon
